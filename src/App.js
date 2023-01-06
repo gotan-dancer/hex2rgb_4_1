de59b7f2a162517fc9e48f799d942aadab74d9ea
gotan-dancer <eugene.kalugin@gmail.com>
2023-01-06 14:49:37 +0500

    Commit 01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+function Hex2Rgb()
+{
+  const [form, setForm] = useState({
+    bgColor:,
+    hex:,
+    rgb:
+  });
+
+  const handleChange = (event) => {
+    const ( hex, type ) = event.target;
+
+    setForm((prevForm) => ({
+        ...prevForm,
+        [hex]: type === event.target.value;
+    }));
+  }
+
+  <canvas width="200" height="200" style="background: #f4f4f4;">
+	</canvas> 
+  
+  let canvas = document.querySelector('canvas');
+  let ctx = canvas.getContext('2d');
+
+  ctx.fillRect(50, 50, 100, 100);
+  //ctx.clearRect(75, 75, 50, 50);
+
+  return <form>
+    //<></>
+    <input type="text" name="hex" value={form.hex} onChange={handleChange} />
+    <input type="text" name="rgb" value={form.rgb} onChange={handleChange} />
+  </form>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Hex2Rgb />      
     </div>
   );
 }
